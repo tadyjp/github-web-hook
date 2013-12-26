0442108ab20cbad00ccecbbc8e54ee1fe9b85a24
tady <tady@zigexn.co.jp>
2013-12-26 17:35:18 +0900

    message format fix

--- web.rb
+++ web.rb
@@ -4,17 +4,15 @@ require 'json'
 
 post '/hook/:token/:room_id' do
 
-  p params
-
-  return 'ng' if params[:token].nil? || params[:room_id].nil?
+  return 'ng' if params[:token].nil? || params[:room_id].nil? || params[:payload].nil?
 
   cw = Chatwork.new(token: params[:token], room_id: params[:room_id])
   payload = JSON.parse(params[:payload])
-  cw.post %Q|[info]
-  [title]Githubにpushされました[/title]
-  User: #{payload['pusher']['name']}
-  Commit: #{payload['commits'].count}
-  compare: #{payload['compare']}
+  cw.post %Q|[info][title]Githubにpushされました[/title]
+  Committer: #{payload['head_commit']['committer']['username']}
+  Commits: #{payload['commits'].count}
+  Compare: #{payload['compare']}
+  Message: #{payload['head_commit']['message']}
   [/info]
   |
 
