acd1159a6005a4289cf232000412e10b0d7fb950
tady <tady@zigexn.co.jp>
2014-02-17 22:19:45 +0900

    use json hook

--- web.rb
+++ web.rb
@@ -2,27 +2,38 @@ require 'sinatra'
 require './chatwork'
 require 'json'
 
-post '/hook/:token/:room_id' do
+def compose_message(title, body)
+  "[info][title]#{title}[/title]#{body}[/info]"
+end
 
-  cw = Chatwork.new(token: params[:token], room_id: params[:room_id])
+post '/hook/:token/:room_id' do
 
   begin
 
-    return 'ng' if params[:token].nil? || params[:room_id].nil? || params[:payload].nil?
+    body = JSON.parse(request.body.read)
+    cw = Chatwork.new(token: params[:token], room_id: params[:room_id])
 
-    payload = JSON.parse(params[:payload])
-    cw.post %Q|[info][title]Githubにpushされました[/title]
-    Committer: #{payload['head_commit']['committer']['username']}
-    Commits: #{payload['commits'].count}
-    Compare: #{payload['compare']}
-    Message: #{payload['head_commit']['message']}
-    [/info]
-    |
+    if params[:token].nil? || params[:room_id].nil? || params[:payload].nil?
+      cw.post compose_message('Error', 'Request error')
+      return 'ng'
+    end
 
-    'ok'
+    case request['X-Github-Event']
+    when 'pull_request'
+      cw.post compose_message("PullRequest '#{body['pull_request']['title']}' #{body['action']} by #{body['pull_request']['user']['login']}", <<-EOS)
+BODY: #{body['pull_request']['body']}
+--
+URL: #{body['pull_request']['url']}
+      EOS
+    when 'pull_request_review_comment'
+      cw.post compose_message("PullRequestComment by #{body['comment']['user']['login']}", <<-EOS)
+BODY: #{body['comment']['body']}
+--
+URL: #{body['comment']['url']}
+      EOS
+    end
 
   rescue => err
-    payload = JSON.parse(params[:payload])
     cw.post %Q|[info][title]github hook error[/title]
     Message: #{err}
     [/info]
